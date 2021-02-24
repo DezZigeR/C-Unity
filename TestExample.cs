7db515f00f68f2b23bcacea49105c9b9c583fb73
DezZigeR <dezziger@mail.ru>
2021-02-24 22:31:38 +0300

    Add files via upload
    test dz

---
+++ TestExample.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Geekbrains.Test
+{
+    internal sealed class TestExample
+    {
+        public delegate void TestDelegate(string value); // 1
+
+        private TestDelegate _testDelegate;   // 2
+
+        private string _money ="9999999999999999999 рублей";
+
+        private Button _button;
+
+        private void NameMethod()
+        {
+            _testDelegate += TestDelegateMethod; // 3
+
+            #region MyRegion
+
+            _testDelegate += t => { Debug.Log(t); };
+            _testDelegate += delegate(string value) { Debug.Log(value); };
+
+            #endregion
+
+            #region MyRegion
+
+            _button.onClick.AddListener(() => Debug.Log(3));
+            _button.onClick.RemoveAllListeners();
+
+            #endregion
+
+            _testDelegate("454");  // 4
+        }
+
+        public void GetMoney(TestDelegate testDelegate)
+        {
+            testDelegate?.Invoke(_money);
+        }
+
+        private void TestDelegateMethod(string value)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
